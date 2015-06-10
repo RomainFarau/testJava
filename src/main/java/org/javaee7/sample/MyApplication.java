fd6817547a1b3c70f048a96970e6d26a5bfdd6fb
Romain Farau <romain.farau@gmail.com>
2015-06-10 12:02:24 +0200

    sdfs

--- src/main/java/org/javaee7/sample/MyApplication.java
+++ src/main/java/org/javaee7/sample/MyApplication.java
@@ -2,7 +2,7 @@ package org.javaee7.sample;
 
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
-//plop nbvcv
+//plop nbvcv vdssqgqsgrgf
 @ApplicationPath("/resources")
 public class MyApplication extends Application {
 
